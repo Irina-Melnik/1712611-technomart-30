63394a08e1f4a6d1372bd7d454bafba812fdb972
Irina-Melnik <IrinaMelnikJune@gmail.com>
2021-03-23 00:22:40 +0300

    Исправления замечаний

--- js/catalog.js
+++ js/catalog.js
@@ -1,23 +1,46 @@
 const modalCartElem = document.querySelector(".modal-cart");
+const topElem = document.querySelector(".catalog");
 const catalogElem = document.querySelector(".catalog");
 const btnBuyElem = document.querySelector(".button-buy");
 const cartCloseBtn = document.querySelector(".modal-cart .modal-close-button");
-const contShopBtn = document.querySelector(".continue-shopping")
+const contShopBtn = document.querySelector(".continue-shopping");
+const modalMakeAnOrder = document.querySelector(".modal-cart .make-an-order");
 
-catalogElem.addEventListener("click", function(evt) {
+let lastFocusedElement;
+
+cartCloseBtn.addEventListener("click", hideModalCart);
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (modalCartElem.classList.contains("modal-show")) {
+        hideModalCart(evt);
+      }
+    }
+});
+
+function hideModalCart(e) {
+    e.preventDefault();
+    modalCartElem.classList.remove("modal-show");
+    lastFocusedElement.focus();
+}
+
+topElem.addEventListener("click", function(evt) {
     evt.preventDefault();
-    console.log("Клик по кнопке «Купить» открывает модальное окно с сообщением о добавлении товара в корзину");
     
     if (evt.target.classList.contains('button-buy')) {
+        console.log("Клик по кнопке «Купить» открывает модальное окно с сообщением о добавлении товара в корзину");
+        lastFocusedElement = evt.target.parentElement.parentElement.parentElement;
         modalCartElem.classList.add("modal-show");
+        modalMakeAnOrder.focus();
     }
 
-    cartCloseBtn.addEventListener("click", function(evt) {
-        evt.preventDefault();
-        modalCartElem.classList.remove("modal-show");
-    });
+    if (evt.target.classList.contains('button-add-to-bookmarks')) {
+        console.log("Клик по кнопке «В закладки» добавляет товар в закладки");
+    }
 
 });
+
+
 
 // catalogElem.addEventListener("click", function(evt) {
 //     evt.preventDefault();
--- js/index.js
+++ js/index.js
@@ -15,8 +15,18 @@ const modalCartElem = document.querySelector(".modal-cart");
 const topElem = document.querySelector(".main-products-top");
 const btnBuyElem = document.querySelector(".button-buy");
 const cartCloseBtn = document.querySelector(".modal-cart .modal-close-button");
-const contShopBtn = document.querySelector(".continue-shopping")
+const contShopBtn = document.querySelector(".continue-shopping");
+const modalMakeAnOrder = document.querySelector(".modal-cart .make-an-order");
 
+const slider = document.querySelector('.slider-wrapper');
+const sliderCtrlPrev = slider.querySelector('.slider-control-prev');
+const sliderCtrlNext = slider.querySelector('.slider-control-next');
+const sliderBottomCtrlPrev = slider.querySelector('.slider-controls>button:first-child');
+const sliderBottomCtrlNext = slider.querySelector('.slider-controls>button:nth-child(2)');
+
+let lastFocusedElement;
+
+let slideChange = setInterval(changeSlide, 4000);
 
 let isStorageSupport = true;
 let storage = "";
@@ -42,6 +52,8 @@ writeUsElem.addEventListener("click", function(evt) {
     evt.preventDefault();
     console.log("Клик по кнопке открывает модальное окно с формой обратной связи");
     modalFbackElem.classList.add("modal-show");
+    modalFbackElem.classList.add("modal-bounce");
+    setTimeout(()=> modalFbackElem.classList.remove("modal-bounce"), 600);
 
     if (storage) {
         login.value = storage;
@@ -69,14 +81,6 @@ fBackCloseBtn.addEventListener("click", function(evt) {
     modalFbackElem.classList.remove("modal-error");
 });
 
-window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      if (modalMapElem.classList.contains("modal-show")) {
-        evt.preventDefault();
-        modalMapElem.classList.remove("modal-show");
-      }
-    }
-});
 
 window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
@@ -85,20 +89,52 @@ window.addEventListener("keydown", function (evt) {
         modalFbackElem.classList.remove("modal-show");
         modalFbackElem.classList.remove("modal-error");
       }
+
+      if (modalMapElem.classList.contains("modal-show")) {
+        evt.preventDefault();
+        modalMapElem.classList.remove("modal-show");
+      }
+
+      if (modalCartElem.classList.contains("modal-show")) {
+        hideModalCart(evt);
+      }
     }
 });
+
+cartCloseBtn.addEventListener("click", hideModalCart);
+
+function hideModalCart(e) {
+    e.preventDefault();
+    modalCartElem.classList.remove("modal-show");
+    lastFocusedElement.focus();
+}
 
 topElem.addEventListener("click", function(evt) {
     evt.preventDefault();
-    console.log("Клик по кнопке «Купить» открывает модальное окно с сообщением о добавлении товара в корзину");
     
     if (evt.target.classList.contains('button-buy')) {
+        console.log("Клик по кнопке «Купить» открывает модальное окно с сообщением о добавлении товара в корзину");
+        lastFocusedElement = evt.target.parentElement.parentElement.parentElement;
         modalCartElem.classList.add("modal-show");
+        modalMakeAnOrder.focus();
     }
 
-    cartCloseBtn.addEventListener("click", function(evt) {
-        evt.preventDefault();
-        modalCartElem.classList.remove("modal-show");
-    });
+    if (evt.target.classList.contains('button-add-to-bookmarks')) {
+        console.log("Клик по кнопке «В закладки» добавляет товар в закладки");
+    }
 
 });
+
+sliderCtrlNext.addEventListener('click', changeSlide);
+sliderCtrlPrev.addEventListener('click', changeSlide);
+sliderBottomCtrlPrev.addEventListener('click', changeSlide);
+sliderBottomCtrlNext.addEventListener('click', changeSlide);
+
+function changeSlide() {
+    slider.classList.toggle('slider-wrapper-1');
+    slider.classList.toggle('slider-wrapper-2');
+    clearInterval(slideChange);
+    slideChange = setInterval(changeSlide, 4000);
+}
+
+
